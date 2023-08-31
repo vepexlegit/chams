c91e75cba50d1c1e2515b764404e6f6c42679a12
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-08-31 19:05:28 +0400

    Create Chams.java

---
+++ src/main/java/de/vepexlegit/chamsdebug/Chams.java
@@ -0,0 +1,15 @@
+package de.vepexlegit.chamsdebug;
+
+public enum Chams {
+    INSTANCE;
+
+    private boolean enabled = true;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
