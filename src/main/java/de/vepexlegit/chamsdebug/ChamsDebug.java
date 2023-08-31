2303ab649bdad1b27cbdc3a469a690bf7e95d6b9
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-08-31 19:06:05 +0400

    Create ChamsDebug.java

---
+++ src/main/java/de/vepexlegit/chamsdebug/ChamsDebug.java
@@ -0,0 +1,16 @@
+package de.vepexlegit.chamsdebug;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ChamsDebug.MODID, version = ChamsDebug.VERSION)
+public class ChamsDebug {
+    public static final String MODID = "chamsdebug";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+    }
+}
